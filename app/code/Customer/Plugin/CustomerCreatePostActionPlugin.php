02d42e0b990d7c5259fd02b0845e348ee06bf2e8
Evgeniy Medvedev <e.v.medvedev@mail.ru>
2019-11-02 18:02:24 +0300

    First commit

---
+++ app/code/Customer/Plugin/CustomerCreatePostActionPlugin.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Smartceo.ru
+ * фильтр спама при регистрации новых пользователей
+ */
+namespace Smartceo\Customer\Plugin;
+
+use Magento\Framework\Controller\Result\RedirectFactory;
+use Magento\Framework\UrlFactory;
+use Magento\Framework\Message\ManagerInterface;
+//use Magento\Framework\Exception\InputException;
+use \Magento\Framework\Exception\NotFoundException;
+
+class CustomerCreatePostActionPlugin {
+
+    public function __construct(
+    UrlFactory $urlFactory, RedirectFactory $redirectFactory, ManagerInterface $messageManager
+    ) {
+        $this->urlModel = $urlFactory->create();
+        $this->resultRedirectFactory = $redirectFactory;
+        $this->messageManager = $messageManager;
+    }
+
+    /**
+     * Фильтрация получаемых данных при регистрации нового пользователя для отсечки спамовых сообщений,
+     * в которых задают адрес почты получателя а в полях имени или фамилии добавляют спамовый текст со ссылками на
+     * сайты.
+     * В контенте определяется наличие ссылок и длина полей ограничивается 30 символами.
+     * 
+     * @param \Magento\Customer\Controller\Account\CreatePost $subject
+     * @throws NotFoundException
+     */
+    public function beforeExecute(\Magento\Customer\Controller\Account\CreatePost $subject/* , \Closure $proceed */) {
+        $firstname = $subject->getRequest()->getParam('firstname');
+        $lastname = $subject->getRequest()->getParam('lastname');
+        
+        preg_match('#\bhttps?://[^,\s()<>]+(?:\([\w\d]+\)|([^,[:punct:]\s]|/))#', $firstname, $match);
+        if (count($match) > 0) {
+            throw new NotFoundException(__('Спамботам здесь не место.'));
+        }
+ 
+        preg_match('#\bhttps?://[^,\s()<>]+(?:\([\w\d]+\)|([^,[:punct:]\s]|/))#', $lastname, $match);
+        if (count($match) > 0) {
+            throw new NotFoundException(__('Спамботам здесь не место.'));
+        }
+        if (strlen(utf8_decode($firstname)) > 30 || strlen(utf8_decode($lastname)) > 30) {
+            throw new NotFoundException(__('Спамботам здесь не место.'));
+        }
+    }
+
+}
